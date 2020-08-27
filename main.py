102577324a6dc45dac8a312553c8b2c7955ad06b
palihabitiya <palihabitiya@protonmail.com>
2020-08-27 14:45:34 +0000

    Add comments

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
+#% of the account risked per trade
 risk = 5
+#Dollars made per dollar risked
 rr = 5
+#% distance until stop loss is executed
 stop = 2
+#% distance until target is reached
 target = stop * rr
+#used to calulate what % of the total account should be placed in a 10x leveraged position
 multiple = risk / 5
+#number of trades taken
 trade_num = 10
+#number of winning trades
 success = 1
 
 risk_table = "Strike rate (%)", "Return over " + str(trade_num) + " trades"
